99413a80d7de173688ddf089b2348f96c59bc183
Anayely <anayelialmonte@gmail.com>
2025-04-08 18:27:10 -0400

    done

---
+++ frontend/secrets.js
@@ -0,0 +1,1 @@
+export const API_KEY = "ybeACTer8zcFxVxcmgpMyc302HOxLIh7";
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 //////////////////////////
 // Imports
 //////////////////////////
-
+const dotenv = require('dotenv');
+dotenv.config();
 const path = require('path');
 const express = require('express');
 
@@ -20,6 +21,25 @@ const app = express();
 const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
+
+
+const trendingGifs = async (req, res) => {
+ const url = `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${process.env.API_KEY}`
+   console.log(`API_KEY: ${process.env.API_KEY}`)
+   console.log(`URL: ${url}`)
+ 
+ try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        res.send(data);
+    
+    } catch(error) {
+    res.status(503).send(error);
+    }
+}
+    
+app.get('/api/gifs', trendingGifs)
 
 //////////////////////////
 // Listener
